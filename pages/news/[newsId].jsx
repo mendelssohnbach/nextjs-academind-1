a5890765cd7417be5e9d0b1cebc855b543127aa2
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-17 08:55:01 +0900

    update: Extracting Dynamic Route Data
    https://gitmoji.dev/

--- pages/news/[newsId].jsx
+++ pages/news/[newsId].jsx
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function DetailPage() {
+  const router = useRouter();
+
+  const newId = router.query.newsId;
+
   return <h1>The Detail Page</h1>;
 }
 
