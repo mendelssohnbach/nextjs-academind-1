f61e6ad92a3801608075d269ae3d00b8b0b7d0a1
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-17 09:06:04 +0900

    update: Linking Between Pages
    https://gitmoji.dev/

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">NetJS IS A Great Framework</Link>
+        </li>
+        <li>Sometig Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
